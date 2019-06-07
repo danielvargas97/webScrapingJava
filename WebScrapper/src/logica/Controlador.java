e462936bb29fc6053296a9297c09c526a79caa55
Daniel Vargas <danielvargas999@gmail.com>
2019-06-07 00:31:59 -0500

    Mas ejemplos para el web scraping

--- WebScrapper/src/logica/Controlador.java
+++ WebScrapper/src/logica/Controlador.java
@@ -5,8 +5,9 @@ public class Controlador {
 	public static void main(String[] args) {
 		Scrapper ejemplo = new Scrapper();
 		//ejemplo.prueba("http://www.zillow.com/homes/for_sale/Denver-CO");
-		ejemplo.prueba("https://listado.mercadolibre.com.co/playstation-4#D[A:playstation%204]");
-			
+		//ejemplo.prueba("https://listado.mercadolibre.com.co/playstation-4#D[A:playstation%204]");
+		//ejemplo.extraerDatosTabla("https://es.wikipedia.org/wiki/Anexo:Libros_m%C3%A1s_vendidos",4);
+		ejemplo.extraerUrlImagen("https://www.bing.com/images/search?q=gatos&go=Buscar&qs=ds&form=QBIR");
 	}
 
 }
--- WebScrapper/src/logica/Scrapper.java
+++ WebScrapper/src/logica/Scrapper.java
@@ -59,7 +59,88 @@ public class Scrapper {
 		
 		
 	}
+	public boolean extraerDatosTabla(String url, int seleccionador) {
+		
+		//Creamos un objeto Document  luego de obtener el html de una url
+		
+		try {
+			document = Jsoup.connect(url).get();
+			String title = document.title();
+			
+			System.out.println("Título:"+title);
+						
+			
+			Elements tablas= document.getElementsByTag("table");
+			
+			//System.out.println(elem.text());
+			
+			int seleccionadorTabla = seleccionador;
+			
+			Elements listaVendidos = tablas.get(seleccionadorTabla).getElementsByTag("tr");
+			System.out.println(listaVendidos.get(0).text()+"\n\n");
+			
+			
+			for(int i = 1; i < listaVendidos.size();i++) {
+				Elements detallesLibro = listaVendidos.get(i).getElementsByTag("td");
+				for(Element elem: detallesLibro) {
+					String data = elem.text();
+					data = limpiarTexto(data);										
+					System.out.println(data);
+				}
+				System.out.println();
+			}
+			
+			return true;
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+			return false;
+			
+		}
+		
+		
+	}
 	
+	
+	public void extraerUrlImagen(String url) {
+		
+		//Creamos un objeto Document  luego de obtener el html de una url
+		
+		try {
+			document = Jsoup.connect(url).get();
+			String title = document.title();
+			
+			System.out.println("Título:"+title);
+						
+			
+			Elements resultadoBusqueda = document.select("img.mimg");
+			
+			for(Element x : resultadoBusqueda) {
+				String src = x.attr("src");				
+				System.out.println(src);
+				
+			}
+			
+			
+			
+			
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+			
+		}
+	}
+	
+	
+	
+	public String limpiarTexto(String texto) {
+		if (texto.matches("[1-9][0-9]* millones\\[[1-9][0-9]*\\]")) {
+			return texto.substring(0, texto.indexOf("["));
+		}
+		else {
+			return texto;
+		}		
+	}
 	
 	
 	
